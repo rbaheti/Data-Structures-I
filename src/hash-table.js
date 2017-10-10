1f1ddb145626eb5b5e335c74de99d4504ddd56ac
Rashmi Baheti <baheti.rash123@gmail.com>
2017-10-10 12:31:07 -0700

    Working on hashtable

--- src/hash-table.js
+++ src/hash-table.js
@@ -8,24 +8,35 @@ class HashTable {
     this.storage = new LimitedArray(this.limit);
     // Do not modify anything inside of the constructor
   }
+
   // Adds the given key, value pair to the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
-
+    const bucket = getIndexBelowMax(key, this.limit);
+    if (bucket === undefined) {
+      this.storage.set(bucket, value);
+    } else {
+      this.storage.set(bucket, value);
+    }
   }
+
   // Removes the key, value pair from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Remove the key, value pair from the bucket
   remove(key) {
-
+    const bucket = getIndexBelowMax(key, this.limit);
+    if (bucket === undefined) return;
+    this.storage.set(bucket, undefined);
   }
+
   // Fetches the value associated with the given key from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Find the key, value pair inside the bucket and return the value
   retrieve(key) {
-
+    const bucket = getIndexBelowMax(key, this.limit);
+    return this.storage.get(bucket);
   }
 }
 
